4330a63fd6030c07f2d3ade46ff2126c3e03d6a9
Pablo Yocupicio <Pabloey@yahoo.com>
2023-01-31 20:55:51 -0700

    Created table for input boxes

--- client/src/App.js
+++ client/src/App.js
@@ -12,37 +12,75 @@ const App = () => {
     box8: 0,
     box9: 0,
     box10: 0,
-    sum: 0
   })
-
-
-  // const [splitBox, setSplitBox] = useState(false)
-
 
   // handleChange functions holds the information put into the input fields.
   const handleChange = (e) => {
     setInputBoxes({ ...inputBoxes, [e.target.name]: parseInt(e.target.value) })
+    console.log(e.target.checked)
   }
 
   return (
     <div>
-      <h1>Sieve Calculator</h1>
-      <form>
-        Is it split? Tick box if yes<input type="checkbox"></input>
-      </form>
-      <form>
-        <input name="box1" onChange={handleChange} type='text'></input> #4<br /><br />
-        <input name="box2" onChange={handleChange} type='text'></input>  8<br /><br />
-        <input name="box3" onChange={handleChange} type='text'></input>  10<br /><br />
-        <input name="box4" onChange={handleChange} type='text'></input>  16<br /><br />
-        <input name="box5" onChange={handleChange} type='text'></input>  30<br /><br />
-        <input name="box6" onChange={handleChange} type='text'></input>  40<br /><br />
-        <input name="box7" onChange={handleChange} type='text'></input>  50<br /><br />
-        <input name="box8" onChange={handleChange} type='text'></input>  100<br /><br />
-        <input name="box9" onChange={handleChange} type='text'></input>  200<br /><br />
-        <input name="box10" onChange={handleChange} type='text'></input>  200+<br /><br />
-      </form>
-      <p>{inputBoxes.box1 + inputBoxes.box2 + inputBoxes.box3 + inputBoxes.box4 + inputBoxes.box5 + inputBoxes.box6 + inputBoxes.box7 + inputBoxes.box8 + inputBoxes.box9 + inputBoxes.box10}</p>
+      <h1>Accumulate Sieve</h1>
+      Is it split? Tick box if yes<input onChange={handleChange} type="checkbox"></input>
+      <table>
+        <tr>
+          <th>Result</th>
+          <th>Weight</th>
+          <th>Sieve</th>
+        </tr>
+        <tr>
+          <td>{inputBoxes.box1}</td>
+          <td><input name="box1" onChange={handleChange}></input></td>
+          <td>#4</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input name="box2" onChange={handleChange}></input></td>
+          <td>8</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input name="box3" onChange={handleChange}></input></td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input name="box4" onChange={handleChange}></input></td>
+          <td>16</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input name="box5" onChange={handleChange}></input></td>
+          <td>30</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input name="box6" onChange={handleChange}></input></td>
+          <td>40</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input name="box7" onChange={handleChange}></input></td>
+          <td>50</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input name="box8" onChange={handleChange}></input></td>
+          <td>100</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input name="box9" onChange={handleChange}></input></td>
+          <td>200</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input name="box10" onChange={handleChange}></input></td>
+          <td>-200</td>
+        </tr>
+      </table>
     </div>
   )
 }
